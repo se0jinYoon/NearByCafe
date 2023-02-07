ad18519a5a2ba492a586575a9eb80a6a03244fc7
cj <toong1203@naver.com>
2023-02-07 15:35:31 +0900

    [add] User 모델 구축

--- User/models.py
+++ User/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from Cafe.models import Location
+    
+class School(models.Model) :
+    location_id = models.ForeignKey('Location', related_name="location_id")
+    school_name = models.CharField(max_length=16)
+    school_email = models.CharField(max_length=16)  #학교 이메일 도메인 변수명 변경
 
-# Create your models here.
+class User(models.Model):
+    school_id = models.ForeignKey(School, related_name="school_id") #학교 식별자[외래키]
+    email_address = models.CharField(max_length=32) #이메일주소
+    user_id = models.CharField(max_length=16) #아이디
+    password = models.CharField(max_length=16) #비밀번호
+    user_realname = models.CharField(max_length=20) #랜덤 생성
+    verified = models.IntegerField(default=0) #학교 인증여부 0 or 1
+
+class Notice(models.Model) :
+    notice_title = models.CharField(max_length=120)
+    notice_time = models.DateTimeField(auto_now_add=True)
+    notice_content = models.TextField()
+    notice_username = models.CharField(max_length=16)
