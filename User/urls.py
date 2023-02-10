3779b2cfd68ed3d4bf64602ef1c647ab75ce3653
oh-bom <spdlqj888888@gmail.com>
2023-02-10 15:16:11 +0900

    [feat]:비밀번호찾기 로직

--- User/urls.py
+++ User/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from . import views
 
 app_name = "User"
 
 urlpatterns = [
+    path("find_pw/",views.find_pw,name="find_pw"),
+    path("find_pw/",views.find_pw2,name="find_pw2"),
     
 ]
--- User/views.py
+++ User/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from .models import Users
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+@csrf_exempt
+def find_pw(request):
+    req=json.loads(request.body)
+    find_email=req['find_email']#사용자가 찾기위해 입력한 이메일
+    
+    try:
+        selected_email=Users.objects.get(email_address=find_email)
+        
+    except:#find_email과 동일한 메일 db에 없을때
+        findOrNot=False
+    else:
+        findOrNot=True
+        
+    finally:
+        context={{"findOrNot":findOrNot, "find_email":find_email}}
+        return JsonResponse(context)
+    
+@csrf_exempt    
+def find_pw2(request):
+    find_email=request.GET.get('find_email')
+    try:
+        selected_email=Users.objects.get(email_address=find_email)
+        
+    except:
+        selected_email=None
+        
+    if find_email is None:
+        overlap="fail"
+    else:overlap="pass"
+    context={'overlap':overlap}
+    return JsonResponse(context)
