4d213bc9ab429538bdc3c53c0cc19e8ac61c8b41
se0jinYoon <tjwls1999@gmail.com>
2023-02-11 22:09:10 +0900

    [feat] 리뷰 생성하기 url, view 생성

--- Review/urls.py
+++ Review/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from . import views
 
 app_name = "Review"
 
 urlpatterns = [
-    
+    path('reivew_create/', views.review_create, name='review_create'),
 ]
--- Review/views.py
+++ Review/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def review_create(request, *args, **kwargs):
+
+    context = {}
+    return render(request, "review_create.html", context=context)
