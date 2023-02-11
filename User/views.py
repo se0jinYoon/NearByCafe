c2f92963c4a691bc79d6e1ebb214cd54d4476411
se0jinYoon <tjwls1999@gmail.com>
2023-02-12 02:59:08 +0900

    [feat] 프로필 페이지 url, view 생성

--- User/views.py
+++ User/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def profile(request, *args, **kwargs):
+
+    context = {}
+    return render(request, "profile.html", context=context)
