bc1fd774da289aa074060915f26fee2fc23b9079
minyou2675 <kymyy2675@gmail.com>
2023-02-12 19:50:14 +0900

    [style] import 모듈 네임 수정

--- User/urls.py
+++ User/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from . import views
+from User import views
+
 app_name = "User"
 
 urlpatterns = [
     path('login/', views.login_page, name='login'),
-   
-    
+    path('profile', views.profile ,name='profile') 
 ]
