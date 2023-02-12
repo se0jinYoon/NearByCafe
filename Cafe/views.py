4edff682153dd445df451ccbdfb196f40d415524
oh-bom <spdlqj888888@gmail.com>
2023-02-12 20:39:19 +0900

    [feat]카페 상세 정보

--- Cafe/views.py
+++ Cafe/views.py
@@ -18,3 +18,24 @@ def main_page(request, *args, **kwargs):
 
     context = {}
     return render(request, "mainpage.html", context=context)
+
+#q.여기서 우리가 클릭한 카페 id를 보내줘야하는데 js로 html에 바로 구겨넣었는데 그거 어케하지?
+def cafe_detail(request,pk,*args,**kwargs):
+    cafe=Cafe.objects(id=pk)
+    
+    #리뷰가 cafe_id를 가지고 있고
+    #나의 cafe_id를 가진 리뷰를 가져오면 all_review
+    all_review=cafe.review_set.all
+    
+    review_cnt=0
+    for i in range(all_review):
+        review_cnt+=1
+    
+    context={
+        "cafe":cafe,
+        "review_cnt":review_cnt,
+           
+    }
+    
+    #url경로 다시 지정
+    return render(request,"cafe_detail.html",context=context)
