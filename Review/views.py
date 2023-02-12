ce5a4712e731869c3adf6c8e092afefc881c88c1
minyou2675 <kymyy2675@gmail.com>
2023-02-12 14:37:50 +0900

    [feat] Review form 추가 / Review create view 추가

--- Review/views.py
+++ Review/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from Review.models import *
+from django.views.decorators.csrf import csrf_exempt
+from .forms import ReviewForm
 # Create your views here.
+@csrf_exempt
+def review_create(request):
+    context = {}
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        else:
+            ctx = {
+                'form':form
+            }
+            return render(request,"review_create.html",ctx)
+   
+    
+    
