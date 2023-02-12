d1a9965fd5fd275ebb740949f1a0d39f91d91c2e
minyou2675 <kymyy2675@gmail.com>
2023-02-12 18:11:37 +0900

    [feat] Review admin 추가

--- Review/admin.py
+++ Review/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from Review.models import Review
 # Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['time','title','content','image','star','mark','cafe_id','user_id']
+    list_display_links = ['time','title','content','image','star','mark','cafe_id','user_id']
