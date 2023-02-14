713726cd8f6363c83c14a7e849e908bc8f4858a7
minyou2675 <kymyy2675@gmail.com>
2023-02-14 17:44:08 +0900

    [feat] 로그인시에만 접근, POST시 탈퇴 로직 구현

--- User/views.py
+++ User/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout as auth_logout
+
+@login_required('/login')
+def delete_user(request):
+    context = {}
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            request.user.delete()
+            auth_logout(request)
+        return redirect('Cafe:main')
+    return render(request,'withdrawal.html',context)
+  
+        
+
 
 # Create your views here.
