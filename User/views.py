07d09ab653cb9bbf25b3e8d94bb8fbe3937a2b3f
minyou2675 <kymyy2675@gmail.com>
2023-02-14 22:06:30 +0900

    [fix]탈퇴 로직 csrf token 추가

--- User/views.py
+++ User/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout as auth_logout
+from django.views.decorators.csrf import csrf_exempt
 
-@login_required('/login')
+# @login_required('/')
+@csrf_exempt
 def delete_user(request):
     context = {}
     if request.user.is_authenticated:
         if request.method == 'POST':
             request.user.delete()
             auth_logout(request)
-        return redirect('Cafe:main')
+        # return redirect('Cafe:main')
+        return render(request,'withdrawal.html',context)
+  
     return render(request,'withdrawal.html',context)
   
         
