94e971bc7e554dbef77b95c5d9413b42f444abd7
oh-bom <spdlqj888888@gmail.com>
2023-02-15 23:29:08 +0900

    [fix]카페상세_좋아요 구현완료

--- static/js/cafe_like.js
+++ static/js/cafe_like.js
@@ -4,8 +4,7 @@ const strip = (string) => {
 cnt=0
 // <button class="review_like d-flex" data-bs-toggle="modal" data-bs-target="#not_login" onclick="onClickLike({{cafe.id}}">♡좋아요</button>
 const onClickLike=async(id,clicked)=>{
-    
-    //좋아요 눌렀을때
+     //좋아요 눌렀을때
     if (cnt==0){
         clicked=true
         cnt+=1
@@ -13,18 +12,23 @@ const onClickLike=async(id,clicked)=>{
     //취소 눌렀을때
     else{
         clicked=false
-        cnt==0
+        cnt=0
     }
 
+    console.log(cnt,clicked)
+   
     const url="/cafe_like/"
     const {data}=await axios.post(url,{
         id,clicked,
     });
 
+    
     likeHandleResponse(data.id,data.clicked);
 }
 
 const likeHandleResponse=(id,clicked)=>{
+    console.log(clicked)
+
     const element=document.querySelector('.review_like');
     const orginhtml=element.innerHTML;
     if(clicked===true){
