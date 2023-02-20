f61e5e1c81d0d150ffcd7f0daab7d971155cda6f
hongcana <hongcana@kakao.com>
2023-02-20 21:27:38 +0900

    [fix] 검색창 없앴음

--- User/email_helper.py
+++ User/email_helper.py
@@ -1,6 +1,7 @@
 from django.core.mail import send_mail
 from django.core.mail import EmailMultiAlternatives
 import threading
+import time
 
 
 class EmailThread(threading.Thread):
@@ -14,16 +15,28 @@ class EmailThread(threading.Thread):
         threading.Thread.__init__(self)
 
     def run(self):
-        print('Email content:', self.subject, self.body, self.from_email,
-              self.recipient_list, self.fail_silently, self.html)
+        print('Email content =-=-=-=-=-=')
+        print(self.subject)
+        print(self.recipient_list)
+
         msg = EmailMultiAlternatives(
             self.subject, self.body, self.from_email, self.recipient_list)
         if self.html:
             msg.attach_alternative(self.html, "text/html")
-        print('Sending email to:', self.recipient_list)
-        msg.send(self.fail_silently)
+        print("now it's time to msg.send ----- ")
+        try:
+            msg.send(self.fail_silently)
+        except Exception as e:
+            print(f"Error sending email: {e}")
 
 
 def send_mail(subject, recipient_list, body='', from_email='nearcafe@naver.com', fail_silently=False, html=None, *args, **kwargs):
-    EmailThread(subject, body, from_email, recipient_list,
-                fail_silently, html).start()
+    print("trying to send the mail with send_mail method...")
+    print(" --- please checking all of infomations are matching --- ")
+    print(subject)
+    print(recipient_list)
+
+    thread = EmailThread(subject, body, from_email,
+                         recipient_list, fail_silently, html)
+    thread.start()
+    time.sleep(1)
